c769168f5b9e87bc44a54664eacddbe7b29d74e2
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-08-03 15:41:56 +0530

    update Encapsulation.java and Interface.java

--- Encapsulation/Encapsulation.java
+++ Encapsulation/Encapsulation.java
@@ -3,5 +3,7 @@ package Encapsulation;
 public class Encapsulation {
     public static void main(String[] args) {
 
+
+        
     }
 }
--- Interface/Interface.java
+++ Interface/Interface.java
@@ -1,4 +1,6 @@
 package Interface;
 
 public interface Interface {
+
+    
 }
