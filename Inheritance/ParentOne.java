c050c7f155f9cc56f8c7e1ef86ba1d0d07aa4ccb
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-08-06 16:07:45 +0530

    update ParentOne.java

--- Inheritance/ParentOne.java
+++ Inheritance/ParentOne.java
@@ -4,37 +4,21 @@ public class ParentOne {
     ParentOne() {
         System.out.println("In Parent Constructor");
     }
+
     void mOne() {
         System.out.println("In Method One");
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // This program referes to inheritance concept in OOP of java
-// Inheritance, When an object aquires all the properties and behabiours of a parent object is known as Inheritance 
+// Inheritance, When an object aquires all the properties and behabiours of a
+// parent object is known as Inheritance
 // It provides code reuabitity to us
 // It represents the parent child relationship
 
-// here in this proram the Child class inherites all the properties of Parent class using (extends) keyword and inherites all
+// here in this proram the Child class inherites all the properties of Parent
+// class using (extends) keyword and inherites all
 // the properties of the Parent class
 
-
-
-
-
-//Suggest any changes if you think that they may add more value to this code and explanation.
+// Suggest any changes if you think that they may add more value to this code
+// and explanation.
