0f3022d3671e22252c769c93b1026f98eca13a43
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-08-09 15:28:08 +0530

    update Encapsulation.java

--- Encapsulation/Encapsulation.java
+++ Encapsulation/Encapsulation.java
@@ -1,8 +1,36 @@
 package Encapsulation;
+class Person {
 
-public class Encapsulation {
-    public static void main(String[] args) {
+    private String name;
+    private int age;
 
-        System.out.println("THis is an example of Encapsulation");
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
+public class Encapsulation {
+    public static void main(String[] args) {
+       
+
+        Person person = new Person();
+        person.setName("John");
+        person.setAge(30);
+
+        
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
+}
+
