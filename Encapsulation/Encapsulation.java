264588fb228f5bb99e706c4eff1e920ed9b57c3a
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-08-11 11:22:20 +0530

    update Encapsulation.java

--- Encapsulation/Encapsulation.java
+++ Encapsulation/Encapsulation.java
@@ -4,6 +4,15 @@ class Person {
 
     private String name;
     private int age;
+    private int mobileNo;
+
+    public int getmobileNo(){
+        return mobileNo;
+    }
+  
+    public void setmobileNo(int mobileNo){
+        this.mobileNo = mobileNo;
+    }
 
     public String getName() {
         return name;
@@ -28,8 +37,10 @@ public class Encapsulation {
         Person person = new Person();
         person.setName("John");
         person.setAge(30);
+        person.setmobileNo(34567012);
 
         System.out.println("Name: " + person.getName());
         System.out.println("Age: " + person.getAge());
+        System.out.println("Mobile NO:"+ person.getmobileNo());
     }
 }
