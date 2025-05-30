a065db1a12f25bc2b20090d5f50f6d9c06b35700
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2025-05-30 14:11:30 +0530

    update solidrectangle.java

--- Patterns/HollowRectangle.java
+++ Patterns/HollowRectangle.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class SolidRectangle {
+public class HollowRectangle {
     public static void main(String[] args) throws IOException {
         System.out.println("Enter the number of row's followed by columns : ");
 
