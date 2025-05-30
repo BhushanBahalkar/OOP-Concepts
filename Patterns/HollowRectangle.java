2f231ecfc725ec4fc55c11c8e40896177c6fa21e
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2025-05-30 14:19:11 +0530

    update HollowRectangle.java

--- Patterns/HollowRectangle.java
+++ Patterns/HollowRectangle.java
@@ -14,8 +14,14 @@ public class HollowRectangle {
         System.out.println(" ");
 
         for (int i = 1; i <= m; i++) {
+
             for (int j = 1; j <= n; j++) {
-                System.out.print("*");
+
+                if (i == 1 || j == 1 || i == m || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println(" ");
         }
