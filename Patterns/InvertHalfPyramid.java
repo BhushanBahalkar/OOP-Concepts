ef0a28f74d6a787e36305b5b2c033ac272dc27fe
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2025-05-30 14:27:18 +0530

    update InvertHalfPyramid.java

--- Patterns/InvertHalfPyramid.java
+++ Patterns/InvertHalfPyramid.java
@@ -14,7 +14,7 @@ public class InvertHalfPyramid {
         // int n = Integer.parseInt(br.readLine());
         System.out.println(" ");
 
-        for(int i = 1; i<= m ; i--){
+        for(int i = m; i>= 1 ; i--){
             for(int j= 1; j<=i; j++){
                 System.out.print("*");
             }
