99fcb2b8d68b422febd34e251743949a218a58f0
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2025-05-30 14:07:32 +0530

    Patterns Addeed

---
+++ Patterns/solidrectangle.java
@@ -0,0 +1,24 @@
+package Patterns;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class solidrectangle {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Enter the number of row's followed by columns : ");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<= n; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+
+    }
+}
