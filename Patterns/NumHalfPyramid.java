df6fa24e4a78264275a322160627bbe114139957
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2025-05-30 14:38:59 +0530

    Humber Half Pyramid Added

---
+++ Patterns/NumHalfPyramid.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NumHalfPyramid {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Enter the number of row's : ");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+        // int n = Integer.parseInt(br.readLine());
+        System.out.println(" ");
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println(" ");
+
+        }
+
+        System.out.println(" ");
+    }
+}
