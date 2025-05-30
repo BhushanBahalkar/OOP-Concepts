eee18a624877a77b3b430edcc4b7045591e01091
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2025-05-30 14:39:59 +0530

    update NumHalfPyramid.java

--- Patterns/NumHalfPyramid.java
+++ Patterns/NumHalfPyramid.java
@@ -15,7 +15,7 @@ public class NumHalfPyramid {
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j);
+                System.out.print(j + " ");
             }
             System.out.println(" ");
 
