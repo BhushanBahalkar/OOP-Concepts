05f5f1c691c08ec6cb0feab765e25c880f90f7f3
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2025-05-31 00:09:26 +0530

    update FloyedTriangle.java

--- Patterns/FloyedTriangle.java
+++ Patterns/FloyedTriangle.java
@@ -10,12 +10,14 @@ public class FloyedTriangle {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int m = Integer.parseInt(br.readLine());
+        int number = 1;
         // int n = Integer.parseInt(br.readLine());
         System.out.println(" ");
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(number + " ");
+                number++;
             }
             System.out.println(" ");
 
