c01c0980a36e59e08a3ed41f907974286ff6039b
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2025-06-01 01:33:22 +0530

    PalindromicPyramid.java added

--- Patterns/PalindromeNumPyra.java
+++ Patterns/PalindromeNumPyra.java
@@ -3,7 +3,7 @@ package Patterns;
 public class PalindromeNumPyra {
     public static void main(String[] args) {
         int n = 5;
-
+//firsthalf
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
@@ -16,5 +16,19 @@ public class PalindromeNumPyra {
             }
             System.out.println(" ");
         }
+        //second half
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j<= i; j++ ){
+                System.out.print(j);
+            }
+            for (int k = i - 1; k >= 1; k--){
+                System.out.print(k);
+            }
+            System.out.println(" ");
+        }
+
     }
 }
