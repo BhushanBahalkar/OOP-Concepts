5483d0b31de54b597ce1116fe3d8940dcbca1cee
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2025-05-31 17:37:11 +0530

    EncapII Added

--- Patterns/ZeroOneTriangle.java
+++ Patterns/ZeroOneTriangle.java
@@ -4,23 +4,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class FloyedTriangle {
+public class ZeroOneTriangle {
     public static void main(String[] args) throws IOException {
-        System.out.println("Enter the number of row's : ");
+        System.out.println("Enter the number of row's followed by columns : ");
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int m = Integer.parseInt(br.readLine());
-        int num = 1;
-        // int n = Integer.parseInt(br.readLine());
+//        int n = Integer.parseInt(br.readLine());
         System.out.println(" ");
-
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(num + " ");
-                num++;
+
+                int sum = i +j ;
+                if (sum % 2 == 0) {
+                    System.out.print("0");
+                } else {
+                    System.out.print("1");
+                }
             }
             System.out.println(" ");
-
         }
 
         System.out.println(" ");
