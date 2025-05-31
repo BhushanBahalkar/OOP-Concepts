316847e2d88123b602f98bdcd00772312a13566b
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2025-06-01 01:38:58 +0530

    PalindromicPyramid.java added

--- Patterns/PalindromicPyramid.java
+++ Patterns/PalindromicPyramid.java
@@ -2,24 +2,25 @@ package Patterns;
 
 public class PalindromicPyramid {
     public static void main(String[] args) {
-        int n =5;
+        int n = 5;
 
-        for (int i = 1; i<=n; i++){
-            //spaces
-            for (int j =1; j<=n-i; j++){
-                System.out.print(" ");
+        for (int i = 1; i <= n; i++) {
+            // Print spaces for centering
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  "); // two spaces for alignment
             }
-            //1st half
-            for (int j = i; j>=1; j--) {
-                System.out.print(j);
-            }
-           //2nd half
 
-            for (int j = 2; j<=i; j++) {
-                System.out.print(j);
+            // 1st half (descending)
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
             }
-            System.out.println();
+
+            // 2nd half (ascending)
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println(); // move to next line
         }
-
     }
 }
