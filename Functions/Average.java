9cb95c559062a444c195c27bf9f5078496458982
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2025-06-02 00:21:34 +0530

    update Average.java

--- Functions/Average.java
+++ Functions/Average.java
@@ -6,8 +6,8 @@ import java.io.InputStreamReader;
 
 public class Average {
 
-    public static void AverageCalculator(int a, int b, int c){
-        int sum = a + b + c ;
+    public static void AverageCalculator(double a, double b, double c){
+        double sum = a + b + c ;
         double average = sum/3.0;
         System.out.println("The average of these three numbers is:" + average);
     }
@@ -16,13 +16,13 @@ public class Average {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter the first number:");
-        int a = Integer.parseInt(br.readLine());
+        double a = Double.parseDouble(br.readLine());
 
         System.out.print("Enter the second number:");
-        int b = Integer.parseInt(br.readLine());
+        double b = Double.parseDouble(br.readLine());
 
         System.out.print("Enter the third number:");
-        int c = Integer.parseInt(br.readLine());
+        double c = Double.parseDouble(br.readLine());
 
         AverageCalculator(a, b, c);
 
---
+++ Functions/OddSum.java
@@ -0,0 +1,7 @@
+package Functions;
+
+public class OddSum {
+
+    
+    
+}
