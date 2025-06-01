717f7102e3d18201007c823bc557bffc60c97aad
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2025-06-02 00:26:31 +0530

    update OddSum.java

--- Functions/OddSum.java
+++ Functions/OddSum.java
@@ -1,7 +1,34 @@
 package Functions;
 
+import java.util.*;
+
 public class OddSum {
 
-    
-    
+    public static void OddSumCalculator(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+        }
+        System.out.println("The sum of odd numbers from 1 to " + n + " is: " + sum);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 2 != 0) {
+                sum = sum + i;
+
+            }
+        }
+
+        sc.close();
+
+    }
+
 }
