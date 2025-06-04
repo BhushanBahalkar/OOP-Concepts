608cb2d0bfda09b5d97874edcc489092bbe9be81
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2025-06-04 23:10:08 +0530

    update Array.java

--- Arrays/Array.java
+++ Arrays/Array.java
@@ -4,6 +4,9 @@ public class Array {
 
     public static void main(String[] args) {
         
+        // Example of a simple array
+        System.out.println("Example of a simple array:");
+
         int[] arr = {1, 2, 3, 4, 5};
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -11,9 +14,12 @@ public class Array {
             
             sum += arr[i];
         }
+
         System.out.println("\nThe sum of the array elements is: " + sum);
         System.out.println("The average of the array elements is: " + sum / arr.length);
         System.out.println(" ");
+
+        // Example of an array with user input
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of the array: ");
@@ -28,6 +34,8 @@ public class Array {
         for (int i = 0; i < userArray.length; i++) {
             System.out.print(userArray[i] + " ");
         }
+        
+        // Example of a string array with user input
         
         String[] strArr = new String[size];
         System.out.println("\n" + "Enter " + size + " strings for the string array:");
